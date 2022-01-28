44b4aa92f34d1f618eb0ed4cb9f11c184ede8b81
Faisal-atiq3 <matiquefaisalrafi@gmail.com>
2022-01-28 22:01:33 +0600

    add

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBiweOTmGYXZC5cmb2qh4G7I9zmWJyc0Vs",
+    authDomain: "assignment-2-96a85.firebaseapp.com",
+    projectId: "assignment-2-96a85",
+    storageBucket: "assignment-2-96a85.appspot.com",
+    messagingSenderId: "912311475562",
+    appId: "1:912311475562:web:8a2e9a031b5ab77bb55913"
+};
+
+export default firebaseConfig;
+  
